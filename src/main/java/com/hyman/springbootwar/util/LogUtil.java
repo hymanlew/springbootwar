0f36d19ac2354ff5352dcd42ce7b5c9c83e4d44e
hymanlew <wohena3689>
2019-12-20 21:57:59 +0800

    del logutil

--- src/main/java/com/hyman/springbootwar/util/LogUtil.java
+++
@@ -1,9 +0,0 @@
-package com.hyman.springbootwar.util;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class LogUtil {
-
-    public static final Logger logger = LoggerFactory.getLogger(LogUtil.class);
-}
